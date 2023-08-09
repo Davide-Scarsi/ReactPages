cc97f4ced5beada368789626cc3b1aeaf4aea11f
Davide Scarsi <davide@cavaliere.io>
2023-08-09 12:27:41 +0200

    fix alla homepage

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ const router = createBrowserRouter([
     element: <App/>,
   },
   {
+    path: "ReactPages",
+    element: <App/>,
+  },
+  {
     path: "pagina1",
     element: <Pagina1/>,
   },
